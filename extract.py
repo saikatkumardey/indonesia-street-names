c11f099de463d8bfc3f5986dc92b537dffe89bfa
Saikat Kumar Dey <deysaikatkumar@gmail.com>
2026-04-12 04:44:51 +0000

    spatial filter: use Indonesia country polygon to exclude Malaysia, SG, PH etc

--- extract.py
+++ extract.py
@@ -1,4 +1,5 @@
 import duckdb
+import urllib.request
 import subprocess
 
 print("Connecting...", flush=True)
@@ -6,6 +7,17 @@ con = duckdb.connect()
 con.execute("INSTALL httpfs; LOAD httpfs;")
 con.execute("INSTALL spatial; LOAD spatial;")
 con.execute("SET s3_region='us-west-2';")
+
+print("Fetching Indonesia boundary...", flush=True)
+url = "https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson"
+urllib.request.urlretrieve(url, "countries.geojson")
+con.execute("""
+    CREATE TABLE indonesia AS
+    SELECT geometry
+    FROM ST_Read('countries.geojson')
+    WHERE properties->>'ADMIN' = 'Indonesia'
+""")
+print("Indonesia boundary loaded", flush=True)
 
 print("Running extraction...", flush=True)
 con.execute("""
@@ -17,17 +29,18 @@ COPY (
             '@[0-9]+$', ''
         ) AS osm_way_id,
         sources[1].dataset AS source_dataset,
-        ST_AsText(geometry) AS geometry_wkt
+        ST_AsText(s.geometry) AS geometry_wkt
     FROM read_parquet(
         's3://overturemaps-us-west-2/release/2026-03-18.0/theme=transportation/type=segment/*',
         hive_partitioning=1
-    )
-    WHERE bbox.xmin BETWEEN 95 AND 141
-      AND bbox.ymin BETWEEN -11 AND 6
+    ) s, indonesia i
+    WHERE s.bbox.xmin BETWEEN 95 AND 141
+      AND s.bbox.ymin BETWEEN -11 AND 6
       AND names.primary IS NOT NULL
       AND length(trim(names.primary)) > 1
       AND names.primary NOT LIKE '%*%'
       AND names.primary != ''
+      AND ST_Within(ST_Centroid(s.geometry), i.geometry)
     ORDER BY street_name
 ) TO 'indonesia_streets.parquet' (FORMAT parquet, COMPRESSION 'zstd');
 """)
